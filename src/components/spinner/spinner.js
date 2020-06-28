5dc7a0a875e35912c464bc6405a3610d50d2a3e1
AnatoliiMikhov <anatoli.mihov@gmail.com>
2020-06-28 20:31:48 +0300

    spinner use styled-component

--- src/components/spinner/spinner.js
+++ src/components/spinner/spinner.js
@@ -1,14 +1,52 @@
 import React from "react";
-import "./spinner.css";
+import styled, {keyframes} from "styled-components";
+
+const spinnerAnimation = keyframes`
+	0% {transform: rotate(0deg)}
+	50% {transform: rotate(180deg)}
+	100% {transform: rotate(360deg)}
+`;
+
+const WrapperSpinnerBlock = styled.div`
+	width: 200px;
+	height: 200px;
+	display: block;
+	position: absolute;
+	top: 50%;
+	left: 50%;
+	transform: translate(-50%, -50%);
+	margin: 0 auto;
+	overflow: hidden;
+	background: none;
+`;
+
+const InnerSpinnerBlock = styled.div`
+	width: 100%;
+	height: 100%;
+	position: relative;
+
+	div {
+		position: absolute;
+		animation: ${spinnerAnimation} 1.5s linear infinite;
+		width: 84px;
+		height: 84px;
+		top: 40%;
+		left: 28%;
+		transform: translate(-50%, -50%);
+		border-radius: 50%;
+		box-shadow: 0 2px 0 0 #16b6df;
+		box-sizing: content-box;
+	}
+`;
 
 const Spinner = () => {
 	return (
-		<div className="loadingio-spinner-eclipse-ce2jxdrybxs">
-			<div className="ldio-wers8tlimgi">
+		<WrapperSpinnerBlock>
+			<InnerSpinnerBlock>
 				<div></div>
-			</div>
-		</div>
-	)
-}
+			</InnerSpinnerBlock>
+		</WrapperSpinnerBlock>
+	);
+};
 
 export default Spinner;
